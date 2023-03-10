fb51ec1807718b0aac69be1ecfe0effeac5b1988
Rebecca Lehew <lehew316@gmail.com>
2023-03-10 10:39:07 -0600

    Added HTML routes and API GET route. Working.

--- server.js
+++ server.js
@@ -8,12 +8,42 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+// Middleware for parsing JSON & urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// GET request
+
+// HTML Routes
+app.get("/", (req, res) => {
+    res.sendFile("./public/index.html");
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+
+// API Routes
+// Get Reqeust
+app.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            res.json(err).status(500);
+        } else {
+            res.send(data).status(200);
+        }
+    });
+})
 
 // POST request
+app.post("/api/notes", (req, res) => {
 
-// DELETE request
+})
+
+// BONUS: DELETE request
+
+
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
