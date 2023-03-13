041720abb87c2605b69b9db9cd80c707a6d775bf
Rebecca Lehew <lehew316@gmail.com>
2023-03-13 17:32:46 -0500

    Debug  prev issues. Work on delete route/BONUS.

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const db = require("./db/db.json");
 const fs = require("fs");
 const path = require("path");
-// const generateUniqueId = require("generate-unique-id");
+
+
+// NPM package to assign random id to each note
+const { v4: uuidv4 } = require("uuid");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,16 +17,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
-
-// HTML Routes
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-})
-
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-})
 
 
 // API Routes
@@ -47,10 +41,11 @@ app.post("/api/notes", (req, res) => {
             res.json(err).status(500);
         } else {
             let newNote = req.body;
+            newNote.id = uuidv4();
             let savedNote = JSON.parse(data);
             savedNote.push(newNote);
 
-            fs.writeFile("/db/db.json", JSON.stringify(savedNote), (err) => {
+            fs.writeFile("./db/db.json", JSON.stringify(savedNote), (err) => {
                 if (err) {
                     res.json(err).status(500);
                 } else {
@@ -62,7 +57,23 @@ app.post("/api/notes", (req, res) => {
 });
 
 // BONUS: DELETE request
-// code starts here...
+app.delete("/api/notes/:id", (req, res) => {
+    let savedNoteID = req.params.id;
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        
+    })
+})
+
+
+// HTML Routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
 
 // Starts the server
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}.`));
