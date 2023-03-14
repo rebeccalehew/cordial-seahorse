3ae30842ce89b0bcc3808b586e4606d416b85b05
Rebecca Lehew <lehew316@gmail.com>
2023-03-13 22:46:28 -0500

    Attempt 2 to debug. Server.js line 66.

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile("./public/index.html");
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 
