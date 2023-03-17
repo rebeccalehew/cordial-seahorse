05c36b071d624c27e5d6224de39e697569f80d88
Rebecca Lehew <lehew316@gmail.com>
2023-03-16 23:25:10 -0500

    possible solution to delete notes

--- server.js
+++ server.js
@@ -55,17 +55,26 @@ app.post("/api/notes", (req, res) => {
 });
 
 // BONUS: DELETE request -- work in progress
-// app.delete("/api/notes/:id", (req, res) => {
-//     // Receives a query parameter that contains the id of a note to delete, reads all notes from the db.json file, removes the note with the given id property, and then rewrites the notes to the db.json file. 
-//     let noteId = req.params.id;
-//     fs.readFile("./db/db.json", "utf-8", (err, data) => {
-//         if (err) {
-//             res.status(500).json(err);
-//         } else {
-//             *
-//         }      
-//     })
-// });
+app.delete("/api/notes/:id", (req, res) => {
+    // Receives a query parameter that contains the id of a note to delete, reads all notes from the db.json file, removes the note with the given id property, and then rewrites the notes to the db.json file. 
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            let noteId = req.params.id;
+            note.id = noteId;
+            deleteNote = JSON.parse(data);
+            deleteNote.splice(noteId);
+            fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(deleteNote), (err) => {
+                if (err) {
+                    res.status(500).json(err);
+                } else {
+                    res.status(200).json(deleteNote);
+                }
+            })
+        }
+    })
+});
 
 
 // HTML Routes
